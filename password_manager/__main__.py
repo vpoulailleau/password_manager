95328a650eda6f7fec3f9b389277b1df5be40dc1
Vincent Poulailleau <vpoulailleau@gmail.com>
2023-09-04 15:45:01 +0200

    ask user input

--- password_manager/__main__.py
+++ password_manager/__main__.py
@@ -1,11 +1,13 @@
+from getpass import getpass
+
 from password_manager.password import generate_passwords
 
 if __name__ == "__main__":
-    # email = input("Entrez votre email : ")
-    # general_password = input("Entrez votre mot de passe principal : ")
-    # url = input("Entrez l'adresse du site pour lequel il faut un mot de passe : ")
-    email = "vpoulailleau@gmail.com"
-    general_password = "password"  # nosec
-    url = "https://www.linkedin.com/signup/cold-join?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Fnotifications%2F%3Ffilter%3Dall&trk=login_reg_redirect"
-
-    generate_passwords(email, general_password, url)
+    email = input("Entrez votre email : ")
+    general_password = getpass("Entrez votre mot de passe principal : ")
+    general_password_2 = getpass("Confirmez votre mot de passe principal : ")
+    if general_password != general_password_2:
+        print("Les mots de passe ne sont pas identiques")
+    else:
+        url = input("Entrez l'adresse du site pour lequel il faut un mot de passe : ")
+        generate_passwords(email, general_password, url)
